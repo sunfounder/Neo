82d2733c55e775a0dc9499c00e47cc39ab8459d8
Lovmong <1967471283@qq.com>
2025-01-14 17:29:06 +0800

    Refactor IMU read and print formatting in example

--- examples/6_1_imu_read.py
+++ examples/6_1_imu_read.py
@@ -5,12 +5,12 @@ my_car = ZeusPi()
 
 try:
     while True:
-        acc_data, gyro_data = my_car.imu.read_all()
+        acc_data, gyro_data = my_car.read_imu()
         print(
-            f"\rACC: {acc_data[0]:^8} {acc_data[1]:^8} {acc_data[2]:^8}"
-            f"  |  GYRO: {gyro_data[0]:^8} {gyro_data[1]:^8} {gyro_data[2]:^8}",
+            f"\rACC: {acc_data[0]:8.5f}g {acc_data[1]:8.5f}g {acc_data[2]:8.5f}g"
+            f"  |  GYRO: {gyro_data[0]:8.5f}deg/s {gyro_data[1]:8.5f}deg/s {gyro_data[2]:8.5f}deg/s",
             end="",
             flush=True)
-        sleep(0.5)
+        sleep(0.1)
 finally:
     my_car.stop()
