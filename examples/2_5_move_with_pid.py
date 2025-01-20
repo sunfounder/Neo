e9495b975624af009321997e0eeddc343d19ffdb
Lovmong <1967471283@qq.com>
2025-01-20 10:29:18 +0800

    add move with pid

---
+++ examples/2_5_move_with_pid.py
@@ -0,0 +1,32 @@
+from zeus_pi import ZeusPi
+from time import sleep
+
+'''
+Obtaining Attitude Data from a 9D IMU and Using PID Control Algorithm to 
+keep the car heading
+'''
+
+my_car = ZeusPi()
+my_car.start_imu_fusion(with_mag=True)
+
+def main():
+    while True:
+        roll, pitch, yaw = my_car.read_euler()
+
+        # my_car.move_with_pid(angle=0, move_power=0, heading=0) # stop and keep heading
+
+        my_car.move_with_pid(angle=0, move_power=50, heading=0) # go straight and keep heading
+
+        # my_car.move_with_pid(angle=-45, move_power=50, heading=45) # move to angle -45 and keep heading facing 45
+
+        print(f'Roll: {roll:.3f}°, Pitch: {pitch:.3f}°, Yaw: {yaw:.3f}° {my_car.origin_heading:.3f}')
+        sleep(0.01)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        my_car.stop()
+        sleep(0.1)
