aae6a5d3a5a2431892447f3edbe7902ea4e6c1ae
Lovmong <1967471283@qq.com>
2025-08-06 15:56:05 +0800

    add  websocket app examples

---
+++ app/app.py
@@ -0,0 +1,5 @@
+## TODO
+## See:
+##    https://github.com/sunfounder/picar-x/blob/v2.0/example/13.app_control.py
+##    https://github.com/sunfounder/sunfounder-controller
+##    https://docs.sunfounder.com/projects/picar-x-v20/en/latest/python/control_by_app.html
