be56923c216c6c943226536cafbf028201eca1c4
susocool <susocool@outlook.com>
2025-09-26 14:14:19 +0800

    fix(compass):Adjust the delay for more direct data feedback. Don't understand TODO.

--- examples/2_1_compass.py
+++ examples/2_1_compass.py
@@ -1,11 +1,12 @@
 from neo import Neo
 from time import sleep
 
-# TODO: setting the placement, range of compass 
+# TODO: setting the placement, range of compass (?)
+
 my_car = Neo()
 
 while True:
     x, y, z, angle = my_car.read_compass()
     angle = my_car.read_compass_angle(filter=True)
     print(f"x: {x:.2f} mGuass   y: {y:.2f} mGuass   z: {z:.2f} mGuass   angle: {angle}°")
-    sleep(0.01)
+    sleep(2)        # sleep(0.01)
