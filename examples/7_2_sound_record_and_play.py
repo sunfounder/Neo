0b5bb15b60deb6da1e8b55c438ea15a77c852c45
susocool <susocool@outlook.com>
2025-11-07 14:11:31 +0800

    fix(sound_record_and_play): Fix redundant dots in recording file names -Fix the issue of redundant dots in file names in recording and playback commands to ensure correct execution of file operations -Improve file name readability

--- examples/7_2_sound_record_and_play.py
+++ examples/7_2_sound_record_and_play.py
@@ -12,8 +12,7 @@ REC_PATH = '/tmp/'
 _time = 5 # seconds
 
 print(f'Recording for {_time} seconds')
-os.system(f'rec -c 1 -r 44100 {REC_PATH}rec_test..wav trim 0 {_time}')
+os.system(f'rec -c 1 -r 44100 {REC_PATH}rec_test.wav trim 0 {_time}')
 
 print('Playing the recorded sound')
-os.system(f'play {REC_PATH}rec_test..wav')
-
+os.system(f'play {REC_PATH}rec_test.wav')
